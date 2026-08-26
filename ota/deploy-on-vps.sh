df2b2b63d6ef7902ffb6c799e05c005bdcbd476f
jia01226 <4zhw6thtw6@privaterelay.appleid.com>
2026-08-26 15:37:55 +0800

    release: point OTA at chat refactor build 20

--- ota/deploy-on-vps.sh
+++ ota/deploy-on-vps.sh
@@ -7,7 +7,7 @@ SITE="/etc/nginx/sites-enabled/gude"
 SNIPPET="/etc/nginx/snippets/ke-ota.conf"
 STAMP="$(date -u +%Y%m%dT%H%M%SZ)"
 BACKUP_DIR="/root/ota-deploy-backups/$STAMP"
-EXPECTED_IPA_SHA256="1a908d736d75bac045e7b0fd24eb910cb25c327d43ac9e7c57e9d380b72da90d"
+EXPECTED_IPA_SHA256="7a1045f102abc4ace785968c8d66c987cfdf23586efa02d827bcd346e71fc6ea"
 INSTALL_URL="https://jiagude.love/ios/2c9715755397490791ffbe1568d2b914/"
 
 for file in index.html manifest.plist KeApp.ipa nginx-location.conf; do
