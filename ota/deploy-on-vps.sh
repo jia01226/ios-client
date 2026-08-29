96f0fe818047b7c8dd7a5e7cb240f480506f5e90
jia01226 <4zhw6thtw6@privaterelay.appleid.com>
2026-08-29 13:01:39 +0800

    chore: update OTA package to build 25

--- ota/deploy-on-vps.sh
+++ ota/deploy-on-vps.sh
@@ -7,7 +7,7 @@ SITE="/etc/nginx/sites-enabled/gude"
 SNIPPET="/etc/nginx/snippets/ke-ota.conf"
 STAMP="$(date -u +%Y%m%dT%H%M%SZ)"
 BACKUP_DIR="/root/ota-deploy-backups/$STAMP"
-EXPECTED_IPA_SHA256="b16d37c0c2f85643bf9864ac5e662b3d2f5e03ee26b905d2a279b259a73d94f0"
+EXPECTED_IPA_SHA256="c0b43a2844ce3384801780e66dc69b17bc132cceefd92257437405f5373a4823"
 INSTALL_URL="https://jiagude.love/ios/2c9715755397490791ffbe1568d2b914/"
 
 for file in index.html manifest.plist KeApp.ipa nginx-location.conf; do
